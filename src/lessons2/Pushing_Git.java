b829b53795c728eeabfbdd6e4bd94d51dd15a316
AryaAlpha <aryakurdo@gmail.com>
2020-11-06 13:28:57 +0300

    getting data from a file

--- src/lessons2/Pushing_Git.java
+++ src/lessons2/Pushing_Git.java
@@ -1,9 +1,33 @@
 package lessons2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Pushing_Git {
 
 	public static void main(String[] args) {
-		System.out.println("I am gonna put this on git yoooo.");
+		Scanner in;
+		try {
+			in = new Scanner(new File("text.txt"));
+		} catch (FileNotFoundException e) {
+			in = new Scanner(System.in);
+		}
+		List<String> alies = new ArrayList<String>();
+		
+		while(in.hasNextLine()) {
+			alies.add(in.nextLine());
+		}
+		
+		for(String ali : alies) {
+			System.out.println("name: " + ali);
+		}
+
+		
+		in.close();
+		
 	}
 
 }
