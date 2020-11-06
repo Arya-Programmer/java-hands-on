148178b7cb2da88d538c36e662b22ccfd54a9a8a
AryaAlpha <aryakurdo@gmail.com>
2020-11-06 15:11:24 +0300

    Queue And Stack

---
+++ src/lessons2/_LinkedList.java
@@ -0,0 +1,33 @@
+package lessons2;
+
+import java.util.LinkedList;
+
+public class _LinkedList {
+
+	public static void main(String[] args) {
+		// this is queue 
+		// first in first out
+		LinkedList<String> name = new LinkedList<String>();
+		name.add("ali");
+		name.add("abas");
+		name.add("fatah");
+		
+		System.out.println(name.remove());
+		System.out.println(name.remove());
+		System.out.println(name.remove());
+		
+		
+		
+		// this is a stack
+		LinkedList<String> stack = new LinkedList<String>();
+		stack.push("ali");
+		stack.push("abas");
+		stack.push("fatah");
+		
+		System.out.println(stack.pop());
+		System.out.println(stack.removeFirst());
+		System.out.println(stack.remove());
+
+	}
+
+}
